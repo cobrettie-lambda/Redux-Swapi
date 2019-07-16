e72cc3832d103324c185d97c81afc092300fb04f
Cobrettie <cobrettiegarner3@gmail.com>
2019-07-15 22:00:53 -0400

    add action types for actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,10 @@
 // we'll need axios
+import axios from 'axios';
+
+// action types
+export const GET_FETCH = 'GET_FETCH'
+export const GET_SUCCESS = 'GET_SUCCESS'
+export const GET_FAIL = 'GET_FAIL'
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
