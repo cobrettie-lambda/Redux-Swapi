776687a4df9e9a9afd0ce903ee83f38bcd1f2291
Cobrettie <cobrettiegarner3@gmail.com>
2019-07-15 22:16:37 -0400

    create action creator for actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,3 @@
-// we'll need axios
 import axios from 'axios';
 
 // action types
@@ -6,8 +5,37 @@ export const GET_FETCH = 'GET_FETCH'
 export const GET_SUCCESS = 'GET_SUCCESS'
 export const GET_FAIL = 'GET_FAIL'
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for success and one for failure
+// action creator 
+export function fetchCharacter() {
+    // action
+    return (dispatch) => {
+        dispatch({ 
+            type: GET_FETCH
+         })
+
+         axios.get('https://swapi.co/api/people')
+            .then((res) => {
+                dispatch({
+                    type: GET_SUCCESS,
+                    payload: res.data
+                })
+            })
+            .catch((err) => {
+                dispatch({
+                    type: GET_FAIL,
+                    payload: err.res.data
+                })
+            })
+    }
+}
+
+// action creator in es6
+// export const fetchCharacter = () => dispatch => {
+//     dispatch({ 
+//         type: GET_FETCH
+//     })
+// }
+
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
